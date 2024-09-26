39c3e4ad3d35f5849ed7dd2fd7ba3068ee39357e
Yasasi Padukka <yasasi327@gmail.com>
2024-09-26 11:32:20 +0530

    Hash the password and implement error handling.

--- routes/attends.js
+++ routes/attends.js
@@ -1,19 +1,26 @@
 const express = require("express");
 const Attends = require("../models/attends");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
-router.post(`/attend/save`, (req, res) => {
-  let newAttends = new Attends(req.body);
-  newAttends.save((err) => {
-    if (err) {
-      return res.status(400).json({
-        error: err,
-      });
-    }
+// Save the password
+router.post("/attend/save", async (req, res) => {
+  try {
+    let newAttends = new Attends(req.body);
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    newAttends.password = await bcrypt.hash(req.body.password, salt);
+
+    await newAttends.save();
     return res.status(200).json({
       success: "Attend Saved success",
     });
-  });
+  } catch (err) {
+    return res.status(400).json({
+      error: err,
+    });
+  }
 });
 
 router.get("/attends", (req, res) => {
